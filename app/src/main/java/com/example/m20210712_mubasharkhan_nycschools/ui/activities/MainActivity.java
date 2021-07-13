7e01444d19769a5acb2cc251c28aacce091d5fd9
mubashar-javed1 <mubasharjaved10@gmail.com>
2021-07-12 20:22:13 -0500

    working to configuration d.i in u.i

--- app/src/main/java/com/example/m20210712_mubasharkhan_nycschools/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/m20210712_mubasharkhan_nycschools/ui/activities/MainActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.m20210712_mubasharkhan_nycschools.NYCSchoolApplication;
 import com.example.m20210712_mubasharkhan_nycschools.R;
+import com.example.m20210712_mubasharkhan_nycschools.di.subcomponent.MainActivitySubComponent;
 
 public class MainActivity extends AppCompatActivity {
+    private MainActivitySubComponent component;
+
+    public MainActivitySubComponent getComponent() {
+        return component;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
