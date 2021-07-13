9944836ac0d5b09f4957f030d6c431cee29daea8
mubashar-javed1 <mubasharjaved10@gmail.com>
2021-07-12 20:35:27 -0500

    MainActivity, Main Fragment and detialFragment are injected using AppComponent

--- app/src/main/java/com/example/m20210712_mubasharkhan_nycschools/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/example/m20210712_mubasharkhan_nycschools/ui/fragments/MainFragment.kt
@@ -1,14 +1,20 @@
 package com.example.m20210712_mubasharkhan_nycschools.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.m20210712_mubasharkhan_nycschools.R
+import com.example.m20210712_mubasharkhan_nycschools.ui.activities.MainActivity
 
 class MainFragment : Fragment() {
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        ( activity as MainActivity ).component.inject(this@MainFragment)
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
