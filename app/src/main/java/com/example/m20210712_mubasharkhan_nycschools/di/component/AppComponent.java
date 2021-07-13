89a2deff921e36022cd17a4e77df02669ddbf53c
mubashar-javed1 <mubasharjaved10@gmail.com>
2021-07-12 21:45:54 -0500

    retrofit module has added

--- app/src/main/java/com/example/m20210712_mubasharkhan_nycschools/di/component/AppComponent.java
+++ app/src/main/java/com/example/m20210712_mubasharkhan_nycschools/di/component/AppComponent.java
@@ -1,12 +1,14 @@
 package com.example.m20210712_mubasharkhan_nycschools.di.component;
 
 import com.example.m20210712_mubasharkhan_nycschools.di.module.MainActivityModule;
+import com.example.m20210712_mubasharkhan_nycschools.di.module.RetrofitModule;
 import com.example.m20210712_mubasharkhan_nycschools.di.subcomponent.MainActivitySubComponent;
 
 import dagger.Component;
 
 @Component(modules = {
-        MainActivityModule.class
+        MainActivityModule.class,
+        RetrofitModule.class
 })
 public interface AppComponent {
     MainActivitySubComponent.Factory mainActivityComponent();
