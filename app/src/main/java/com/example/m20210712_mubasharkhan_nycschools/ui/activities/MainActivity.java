23bd165e72b71c2be1a71fcc80ea6964f0d81442
mubashar-javed1 <mubasharjaved10@gmail.com>
2021-07-12 19:15:57 -0500

    main activity package changed, main fragment and detail fragments created

--- app/src/main/java/com/example/m20210712_mubasharkhan_nycschools/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/m20210712_mubasharkhan_nycschools/ui/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.m20210712_mubasharkhan_nycschools;
+package com.example.m20210712_mubasharkhan_nycschools.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.m20210712_mubasharkhan_nycschools.R;
 
 public class MainActivity extends AppCompatActivity {
 
